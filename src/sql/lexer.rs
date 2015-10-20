4ddf4d5d3328e82fc9614dd64891b59ccebe3188
Alex-Diez <alexvic.amatory@gmail.com>
2015-10-20 20:31:18 +0300

    lexer re-factoring

--- src/sql/lexer.rs
+++ src/sql/lexer.rs
@@ -4,28 +4,27 @@ use std::collections::HashMap;
 
 pub struct Scanner<'a> {
     src: &'a str,
-    vertical_white_spaces: Vec<char>,
+    horizontal_white_spaces: Vec<char>,
     single_special_chars: Vec<char>,
-    double_special_chars: HashMap<char, char>,
-    multi_special_chars: Vec<char>,
+    double_special_chars: HashMap<char, Vec<char>>,
 }
 
 impl<'a> Scanner<'a> {
 
     pub fn new(src: &'a str) -> Scanner {
-        let mut double_special_chars = HashMap::with_capacity(6);
-        double_special_chars.insert('=', '=');
-        double_special_chars.insert('/', '*');
-        double_special_chars.insert('*', '/');
-        double_special_chars.insert('-', '-');
-        double_special_chars.insert('>', '=');
-        double_special_chars.insert('!', '=');
+        let mut double_special_chars = HashMap::with_capacity(7);
+        double_special_chars.insert('=', vec!['=']);
+        double_special_chars.insert('/', vec!['*']);
+        double_special_chars.insert('*', vec!['/']);
+        double_special_chars.insert('-', vec!['-']);
+        double_special_chars.insert('>', vec!['=']);
+        double_special_chars.insert('!', vec!['=']);
+        double_special_chars.insert('<', vec!['=', '>']);
         Scanner {
                 src: src,
-                vertical_white_spaces: vec![' ', '\t'],
+                horizontal_white_spaces: vec![' ', '\t'],
                 single_special_chars: vec!['(', ')', '\'', ';', '.', '+'],
                 double_special_chars: double_special_chars,
-                multi_special_chars: vec!['*', '-', '/'],
         }
     }
 
@@ -39,38 +38,31 @@ impl<'a> Scanner<'a> {
 
     fn check_double_spec_chars(&self, first_char: &char, next_char: &char) -> bool {
         match self.double_special_chars.get(first_char) {
-            Some(c) => *c == *next_char,
+            Some(chars) => (*chars).contains(next_char),
             None => false
         }
     }
 
-    fn next_lexem(&mut self) -> Option<&'a str> {
-        let first_char = self.first_char();
-        let option_nth_char = self.nth_char(1);
-        let index = if first_char.is_alphabetic() || first_char.is_digit(10) {
+    fn find_delimeter_index(&self, first_char: &char, option_nth_char: &Option<char>) -> usize {
+        if option_nth_char.is_none() {
+            1
+        }
+        else if first_char.is_alphabetic() || first_char.is_digit(10) {
             self.src.chars().take_while(
-                |c| !self.vertical_white_spaces.contains(c)
+                |c| !self.horizontal_white_spaces.contains(c)
                     && *c != '\n'
                     && !self.single_special_chars.contains(c)
-                    && !self.multi_special_chars.contains(c)
+                    && !self.double_special_chars.contains_key(c)
                 ).count()
         }
         else if self.vertical_white_spaces.contains(&first_char) {
-            self.src.chars().take_while(|c| self.vertical_white_spaces.contains(c)).count()
+            self.src.chars().take_while(|c| self.horizontal_white_spaces.contains(c)).count()
         }
         else if self.single_special_chars.contains(&first_char) {
             1
         }
-        else if self.double_special_chars.contains_key(&first_char) && option_nth_char.is_some() {
+        else if self.double_special_chars.contains_key(&first_char) {
             if self.check_double_spec_chars(&first_char, &(option_nth_char.unwrap())) {
-                2
-            }
-            else {
-                1
-            }
-        }
-        else if first_char == '<' {
-            if  option_nth_char.unwrap() == '>' || option_nth_char.unwrap() == '=' {
                 2
             }
             else {
@@ -79,11 +71,23 @@ impl<'a> Scanner<'a> {
         }
         else {
             self.src.chars().take_while(|c| *c == '\n').count()
-        };
-        let result = &(self.src)[0..index];
+        }
+    }
+
+    fn get_lexem(&self, index: usize) -> &'a str {
+        &(self.src)[0..index]
+    }
+
+    fn resize_source(&mut self, index: usize) {
         let src_len = self.src.len();
         self.src = &(self.src)[index..src_len];
-        Option::Some(result)
+    }
+
+    fn next_lexem(&mut self) -> Option<&'a str> {
+        let index = self.find_delimeter_index(&self.first_char(), &self.nth_char(1));
+        let lexem = self.get_lexem(index);
+        self.resize_source(index);
+        Option::Some(lexem)
     }
 }
 
