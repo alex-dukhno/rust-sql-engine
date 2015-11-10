afc6d6d362e591562dbf95b8b4847d5520f6ee9f
Alex-Diez <alexvic.amatory@gmail.com>
2015-11-10 18:34:42 +0200

    lexer development

--- src/sql/lexer.rs
+++ src/sql/lexer.rs
@@ -2,6 +2,8 @@ use std::option::Option;
 use std::str::Chars;
 use std::iter::Peekable;
 
+const SIGN: char = '~';
+
 #[derive(PartialEq, Debug)]
 pub enum Token {
     NewLine,
@@ -16,23 +18,23 @@ pub struct Lexer<'a> {
 
 impl <'a> Lexer<'a> {
 
-    pub fn new(sql: &'a str) -> Lexer {
-        Lexer { src: sql.chars().peekable() }
+    pub fn new(sql: &'a str) -> Lexer<'a> {
+        Lexer {
+            src: sql.chars().peekable()
+        }
     }
 
     pub fn is_empty(&mut self) -> bool {
         self.src.peek() == None
     }
 
-    fn same_symbol_group(c: &Option<char>, iter: &mut Peekable<Chars<'a>>) -> bool {
-        let next = iter.peek();
-        if c.as_ref() != next {
-            return match *c {
-                Some(' ') | Some('\t') => Some(&' ') == next || Some(&'\t') == next,
-                Some(_) | None => false
-            }
+    fn has_same_group(symbol: &Option<char>, next_symbol: &Option<&char>, collector: &mut Vec<char>) -> bool {
+        match (*symbol, *next_symbol) {
+            (Some(' ') , Some(&' ')) | (Some('\t') , Some(&'\t')) | (Some(' '), Some(&'\t')) | (Some('\t'), Some(&' ')) => true,
+            (Some('\n'), Some(&'\n')) => true,
+            (Some('a'...'z'), Some(v @ &'a'...'z')) => { collector.push(*v); true },
+            (Some(_), Some(_)) | (Some(_), None) | (None, Some(_)) | (None, None) => false
         }
-        true
     }
 }
 
@@ -45,12 +47,18 @@ impl <'a> Iterator for Lexer<'a> {
             return None;
         }
         let c = self.src.next();
-        while Lexer::same_symbol_group(&c, &mut self.src) {
+        let mut collector = Vec::new();
+        collector.push(c.unwrap());
+        loop {
+            if !Lexer::has_same_group(&c, &(self.src.peek()), &mut collector) {
+                break;
+            }
             self.src.next();
         }
         match c {
             Some(' ') | Some('\t')  => Some(Token::WhiteSpace),
             Some('\n') => Some(Token::NewLine),
+            Some('a'...'z') => Some(Token::KeyWord(collector.iter().map(|c| *c).collect())),
             Some(_) | None => None
         }
     }
