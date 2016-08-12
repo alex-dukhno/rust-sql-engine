65205f085b909341ac5d6daae990d151dfe04e33
Alex-Diez <alexvic.amatory@gmail.com>
2016-08-12 17:37:03 +0300

    simplify lexer

--- src/sql/sql.rs
+++
@@ -1,4 +0,0 @@
-pub mod lexer;
-// pub mod parser;
-// pub mod ast;
-// pub mod error;
--- tests/unit_tests.rs
+++ tests/unit_tests.rs
@@ -1,8 +1,8 @@
 #![feature(plugin,const_fn)]
 #![plugin(stainless)]
 pub mod lexer_unit_tests;
-// pub mod evaluator_unit_tests;
-// pub mod parser_unit_tests;
-// pub mod ast_unit_tests;
+
+#[macro_use(expect)]
+extern crate expectest;
 
 extern crate sql;
