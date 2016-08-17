d93311c25969cfed2109a44c0b5d16f7e30d1e3b
Alex-Diez <alexvic.amatory@gmail.com>
2016-08-17 15:46:15 +0100

    add string and number lexems

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,10 +1,14 @@
-use self::Token::{Identifier, LeftParenthesis, RightParenthesis, Comma, SingleQuote, EqualSign, Semicolon, Asterisk};
-
-const DELIMETERS: &'static str = "(),\';=* ";
+use std::iter::Peekable;
+use std::str::Chars;
+use self::Token::{Identifier, LeftParenthesis, RightParenthesis, Comma, SingleQuote, EqualSign, Semicolon, Asterisk, StringValue, NumberValue};
 
 #[derive(Debug,PartialEq)]
 pub enum Token {
     Identifier(String),
+
+    NumberValue(String),
+    StringValue(String),
+
     LeftParenthesis,
     RightParenthesis,
     Comma,
@@ -15,48 +19,101 @@ pub enum Token {
 }
 
 pub trait Tokenizer {
-    
-    fn tokenize(&self) -> Vec<Token>;
+
+    fn tokenize(&self) -> Result<Vec<Token>, String>;
 }
 
 impl Tokenizer for String {
 
-    fn tokenize(&self) -> Vec<Token> {
-        let mut tokens = vec![];
-        let mut end = 0;
-        let mut start = end;
-        for c in self.chars() {
-            if DELIMETERS.contains(c) {
-                if start < end {
-                    tokens.push(Identifier(self.chars().skip(start).take(end - start).collect::<String>()));
-                }
-                end += 1;
-                start = end;
-                if let Some(token) = char_to_token(c) {
-                    tokens.push(token);
-                }
-            }
-            else {
-                end += 1;
-            }
-        }
-        if start < end {
-            tokens.push(Identifier(self.chars().skip(start).take(end - start).collect::<String>()));
-        }
-        tokens
+    fn tokenize(&self) -> Result<Vec<Token>, String> {
+        tokenize_expression(&mut self.chars().peekable())
     }
-    
 }
 
-fn char_to_token(c: char) -> Option<Token> {
+fn tokenize_expression(chars: &mut Peekable<Chars>) -> Result<Vec<Token>, String> {
+    let mut tokens = vec![];
+    loop {
+        match chars.peek().cloned() {
+            Some(' ') => { chars.next(); },
+            Some('\'') => { chars.next(); tokens.push(string_token(&mut chars.by_ref())); },
+            Some('a'...'z') => { tokens.push(ident_token(&mut chars.by_ref())); },
+            Some('0'...'9') => { tokens.push(try!(num_token(&mut chars.by_ref()))); },
+            Some(c) => { chars.next(); tokens.push(try!(char_to_token(c))); },
+            None => break,
+        }
+    }
+    Ok(tokens)
+}
+
+fn ident_token(chars: &mut Peekable<Chars>) -> Token {
+    let mut token = String::default();
+    loop {
+        match chars.peek().cloned() {
+            Some(c @ 'a'...'z') | Some(c @ '_') | Some(c @ '0'...'9') => {
+                chars.next();
+                token.push(c);
+            },
+            Some(_) | None => break,
+        }
+    }
+    Identifier(token)
+}
+
+fn num_token(chars: &mut Peekable<Chars>) -> Result<Token, String> {
+    let mut num = String::default();
+    let mut float_point = false;
+    loop {
+        match chars.peek().cloned() {
+            Some(d @ '0'...'9') => {
+                chars.next();
+                num.push(d);
+            },
+            Some('.') => {
+                if !float_point {
+                    chars.next();
+                    num.push('.');
+                    float_point = true;
+                }
+                else {
+                    return Err("Number format error".to_owned());
+                }
+            },
+            Some(_) | None => break,
+        }
+    }
+    Ok(NumberValue(num))
+}
+
+fn string_token(chars: &mut Peekable<Chars>) -> Token {
+    let mut string = String::default();
+    loop {
+        match chars.peek().cloned() {
+            Some('\'') => {
+                chars.next();
+                match chars.peek().cloned() {
+                    Some('\'') => {
+                        chars.next();
+                        string.push('\'');
+                    },
+                    _ => break,
+                }
+            },
+            Some(c) => { chars.next(); string.push(c); },
+            None => break,
+        }
+    }
+    StringValue(string)
+}
+
+fn char_to_token(c: char) -> Result<Token, String> {
     match c {
-        '('     => Some(LeftParenthesis),
-        ')'     => Some(RightParenthesis),
-        ','     => Some(Comma),
-        '\''    => Some(SingleQuote),
-        ';'     => Some(Semicolon),
-        '='     => Some(EqualSign),
-        '*'     => Some(Asterisk),
-        _       => None
+        '('     => Ok(LeftParenthesis),
+        ')'     => Ok(RightParenthesis),
+        ','     => Ok(Comma),
+        '\''    => Ok(SingleQuote),
+        ';'     => Ok(Semicolon),
+        '='     => Ok(EqualSign),
+        '*'     => Ok(Asterisk),
+        _       => Err(format!("Unexpected character - {:?}", c)),
     }
 }
--- test/lexer_tests.rs
+++ test/lexer_tests.rs
@@ -1,45 +1,62 @@
-pub use expectest::prelude::be_equal_to;
+pub use expectest::prelude::{be_ok, be_err};
 
 pub use sql::lexer::Tokenizer;
-pub use sql::lexer::Token::{self, Identifier, LeftParenthesis, RightParenthesis, Comma, SingleQuote, Semicolon, EqualSign, Asterisk};
+pub use sql::lexer::Token::{self, Identifier, LeftParenthesis, RightParenthesis, Comma, SingleQuote, Semicolon, EqualSign, Asterisk, NumberValue, StringValue};
 
 describe! lexer {
 
     describe! lexems {
 
-
         it "emits None when given an empty string" {
             expect!("".to_owned().tokenize())
-                .to(be_equal_to(vec![]));
+                .to(be_ok().value(vec![]));
         }
 
         it "emits identifier token when given a single word string" {
             expect!("word".to_owned().tokenize())
-                .to(be_equal_to(vec![Identifier("word".to_owned())]));
+                .to(be_ok().value(vec![Identifier("word".to_owned())]));
         }
 
         it "emits identifiers when given string of words" {
-            expect!("This is a sentence".to_owned().tokenize())
-                .to(be_equal_to(vec![Identifier("This".to_owned()), Identifier("is".to_owned()), Identifier("a".to_owned()), Identifier("sentence".to_owned())]));
+            expect!("this is a sentence".to_owned().tokenize())
+                .to(be_ok().value(vec![Identifier("this".to_owned()), Identifier("is".to_owned()), Identifier("a".to_owned()), Identifier("sentence".to_owned())]));
+        }
+
+        it "emits number token when given number" {
+            expect!("5".to_owned().tokenize())
+                .to(be_ok().value(vec![NumberValue("5".to_owned())]));
+        }
+
+        it "emits number token when given number with float point" {
+            expect!("2.01".to_owned().tokenize())
+                .to(be_ok().value(vec![NumberValue("2.01".to_owned())]));
+        }
+
+        it "emits error when given number with two delimeters" {
+            expect!("2.0.1".to_owned().tokenize())
+                .to(be_err().value("Number format error"));
+        }
+
+        it "escapes single quote inside string token" {
+            expect!("\'str\'\'str\'".to_owned().tokenize())
+                .to(be_ok().value(vec![StringValue("str\'str".to_owned())]));
         }
     }
 
-    describe! sql_lexems {
+    describe! sql_statements {
 
         it "emits lexems of sql insert statement" {
-            expect!("insert into table1 values(val1, 'val2');".to_owned().tokenize())
-                .to(be_equal_to(
+            expect!("insert into table values(10, 'str');".to_owned().tokenize())
+                .to(be_ok().value(
                     vec![
                         Identifier("insert".to_owned()),
                         Identifier("into".to_owned()),
-                        Identifier("table1".to_owned()),
+                        Identifier("table".to_owned()),
                         Identifier("values".to_owned()),
                         LeftParenthesis,
-                        Identifier("val1".to_owned()),
+                        NumberValue("10".to_owned()),
                         Comma,
-                        SingleQuote,
-                        Identifier("val2".to_owned()),
-                        SingleQuote,
+                        StringValue("str".to_owned()),
                         RightParenthesis,
                         Semicolon
                     ]
@@ -48,7 +65,7 @@ describe! lexer {
 
         it "emits lexems of sql delete statement" {
             expect!("delete from table_name where col_name = 'five';".to_owned().tokenize())
-                .to(be_equal_to(
+                .to(be_ok().value(
                     vec![
                         Identifier("delete".to_owned()),
                         Identifier("from".to_owned()),
@@ -56,41 +73,37 @@ describe! lexer {
                         Identifier("where".to_owned()),
                         Identifier("col_name".to_owned()),
                         EqualSign,
-                        SingleQuote,
-                        Identifier("five".to_owned()),
-                        SingleQuote,
+                        StringValue("five".to_owned()),
                         Semicolon
                     ]
                 ));
         }
 
         it "emits lexems of sql update statement" {
-            expect!("update table_name set col1=val1,col2='val2' where col3=val3".to_owned().tokenize())
-                .to(be_equal_to(
+            expect!("update table_name set col_one=val1,col_two='val2' where col_three=3".to_owned().tokenize())
+                .to(be_ok().value(
                     vec![
                         Identifier("update".to_owned()),
                         Identifier("table_name".to_owned()),
                         Identifier("set".to_owned()),
-                        Identifier("col1".to_owned()),
+                        Identifier("col_one".to_owned()),
                         EqualSign,
                         Identifier("val1".to_owned()),
                         Comma,
-                        Identifier("col2".to_owned()),
+                        Identifier("col_two".to_owned()),
                         EqualSign,
-                        SingleQuote,
-                        Identifier("val2".to_owned()),
-                        SingleQuote,
+                        StringValue("val2".to_owned()),
                         Identifier("where".to_owned()),
-                        Identifier("col3".to_owned()),
+                        Identifier("col_three".to_owned()),
                         EqualSign,
-                        Identifier("val3".to_owned())
+                        NumberValue("3".to_owned())
                     ]
                 ));
         }
 
         it "emits lexems of sql select statement" {
             expect!("select count(*),count(col1)from table_name".to_owned().tokenize())
-                .to(be_equal_to(
+                .to(be_ok().value(
                     vec![
                         Identifier("select".to_owned()),
                         Identifier("count".to_owned()),
